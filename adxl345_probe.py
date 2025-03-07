a4404606027f4ede22b7b806aa1ee01c6058d84c
Zeanon <thezeanon@gmail.com>
2025-03-07 16:29:13 +0100

    .

--- adxl345_probe.py
+++ adxl345_probe.py
@@ -10,7 +10,7 @@ REG_INT_SOURCE = 0x30
 DUR_SCALE = 0.000625  # 0.625 msec / LSB
 TAP_SCALE = 0.0625 * adxl345.FREEFALL_ACCEL  # 62.5mg/LSB * Earth gravity in mm/s**2
 
-ADXL345_REST_TIME = .1
+ADXL345_REST_TIME = 0.1
 
 
 class ADXL345Endstop:
@@ -18,7 +18,10 @@ class ADXL345Endstop:
         self.adxl345probe = adxl345probe
         self.printer = adxl345probe.printer
         self.mcu_endstop = None
-        self.stepper_enable = self.printer.load_object(self.adxl345probe.config, "stepper_enable")
+        self.stepper_enable = self.printer.load_object(
+            self.adxl345probe.config, "stepper_enable"
+        )
+        self.gcode = self.printer.lookup_object("gcode")
 
     def setup_pin(self, pin_type, pin_params):
         # Validate pin
@@ -40,7 +43,14 @@ class ADXL345Endstop:
     def handle_homing_move_begin(self, hmove):
         if self.mcu_endstop not in hmove.get_mcu_endstops():
             return
-        self.printer.lookup_object("toolhead").dwell(self.adxl345probe.stepper_enable_dwell_time)
+        for es in hmove.get_mcu_endstops():
+            for stepper in es[0].get_steppers():
+                self.gcode.respond_info(stepper.get_name())
+                self.stepper_enable.motor_debug_enable(stepper.get_name(), True)
+
+        self.printer.lookup_object("toolhead").dwell(
+            self.adxl345probe.stepper_enable_dwell_time
+        )
         self.adxl345probe.probe_prepare(hmove, xy_homing=True)
 
     def handle_homing_move_end(self, hmove):
@@ -49,43 +59,49 @@ class ADXL345Endstop:
         self.adxl345probe.probe_finish(hmove, xy_homing=True)
 
 
-
 class ADXL345Probe:
     def __init__(self, config):
         self.config = config
         self.printer = config.get_printer()
-        gcode_macro = self.printer.load_object(config, 'gcode_macro')
-        self.activate_gcode = gcode_macro.load_template(config, 'activate_gcode', '')
-        self.deactivate_gcode = gcode_macro.load_template(config, 'deactivate_gcode', '')
-        int_pin = config.get('int_pin').strip()
+        gcode_macro = self.printer.load_object(config, "gcode_macro")
+        self.activate_gcode = gcode_macro.load_template(config, "activate_gcode", "")
+        self.deactivate_gcode = gcode_macro.load_template(
+            config, "deactivate_gcode", ""
+        )
+        int_pin = config.get("int_pin").strip()
         self.inverted = False
         self.is_measuring = False
         self._in_multi_probe = False
-        if int_pin.startswith('!'):
+        if int_pin.startswith("!"):
             self.inverted = True
             int_pin = int_pin[1:].strip()
-        if int_pin != 'int1' and int_pin != 'int2':
-            raise config.error('int_pin must specify one of int1 or int2 pins')
-        probe_pin = config.get('probe_pin')
-        adxl345_name = config.get('chip', 'adxl345')
-        self.int_map = 0x40 if int_pin == 'int2' else 0x0
-        self.tap_thresh = config.getfloat('tap_thresh', 5000, minval=TAP_SCALE, maxval=100000.)
-        self.tap_dur = config.getfloat('tap_dur', 0.01, above=DUR_SCALE, maxval=0.1)
-        self.position_endstop = config.getfloat('z_offset')
-        self.disable_fans = [fan.strip() for fan in config.get("disable_fans", "").split(",") if fan]
+        if int_pin != "int1" and int_pin != "int2":
+            raise config.error("int_pin must specify one of int1 or int2 pins")
+        probe_pin = config.get("probe_pin")
+        adxl345_name = config.get("chip", "adxl345")
+        self.int_map = 0x40 if int_pin == "int2" else 0x0
+        self.tap_thresh = config.getfloat(
+            "tap_thresh", 5000, minval=TAP_SCALE, maxval=100000.0
+        )
+        self.tap_dur = config.getfloat("tap_dur", 0.01, above=DUR_SCALE, maxval=0.1)
+        self.position_endstop = config.getfloat("z_offset")
+        self.disable_fans = [
+            fan.strip() for fan in config.get("disable_fans", "").split(",") if fan
+        ]
 
         self.adxl345 = self.printer.lookup_object(adxl345_name)
-        self.next_cmd_time = self.action_end_time = 0.
+        self.next_cmd_time = self.action_end_time = 0.0
         # Create an "endstop" object to handle the sensor pin
-        ppins = self.printer.lookup_object('pins')
-        pin_params = ppins.lookup_pin(probe_pin, can_invert=True,
-                                      can_pullup=True)
-        mcu = pin_params['chip']
-        self.mcu_endstop = mcu.setup_pin('endstop', pin_params)
-        self.enable_x_homing = config.getboolean('enable_x_homing', False)
-        self.enable_y_homing = config.getboolean('enable_y_homing', False)
-        self.enable_probe = config.getboolean('enable_probe', True)
-        self.stepper_enable_dwell_time = config.getfloat('stepper_enable_dwell_time', 0.1)
+        ppins = self.printer.lookup_object("pins")
+        pin_params = ppins.lookup_pin(probe_pin, can_invert=True, can_pullup=True)
+        mcu = pin_params["chip"]
+        self.mcu_endstop = mcu.setup_pin("endstop", pin_params)
+        self.enable_x_homing = config.getboolean("enable_x_homing", False)
+        self.enable_y_homing = config.getboolean("enable_y_homing", False)
+        self.enable_probe = config.getboolean("enable_probe", True)
+        self.stepper_enable_dwell_time = config.getfloat(
+            "stepper_enable_dwell_time", 0.1
+        )
         # Add wrapper methods for endstops
         self.get_mcu = self.mcu_endstop.get_mcu
         self.add_stepper = self.mcu_endstop.add_stepper
@@ -94,8 +110,14 @@ class ADXL345Probe:
         self.home_wait = self.mcu_endstop.home_wait
         self.query_endstop = self.mcu_endstop.query_endstop
         # Register commands and callbacks
-        self.gcode = self.printer.lookup_object('gcode')
-        self.gcode.register_mux_command("SET_ACCEL_PROBE", "CHIP", None, self.cmd_SET_ACCEL_PROBE, desc=self.cmd_SET_ACCEL_PROBE_help)
+        self.gcode = self.printer.lookup_object("gcode")
+        self.gcode.register_mux_command(
+            "SET_ACCEL_PROBE",
+            "CHIP",
+            None,
+            self.cmd_SET_ACCEL_PROBE,
+            desc=self.cmd_SET_ACCEL_PROBE_help,
+        )
         if self.enable_probe:
             self.printer.add_object("probe", self)
         if self.enable_x_homing:
@@ -104,9 +126,10 @@ class ADXL345Probe:
         if self.enable_x_homing:
             y_endstop = ADXL345Endstop(self)
             ppins.register_chip("adxl_probe_y", y_endstop)
-        self.printer.register_event_handler('klippy:connect', self.init_adxl)
-        self.printer.register_event_handler('klippy:mcu_identify', self.handle_mcu_identify)
-
+        self.printer.register_event_handler("klippy:connect", self.init_adxl)
+        self.printer.register_event_handler(
+            "klippy:mcu_identify", self.handle_mcu_identify
+        )
 
     def init_adxl(self):
         chip = self.adxl345
@@ -120,10 +143,10 @@ class ADXL345Probe:
         chip.set_reg(REG_DUR, int(self.tap_dur / DUR_SCALE))
 
     def handle_mcu_identify(self):
-        self.phoming = self.printer.lookup_object('homing')
-        kin = self.printer.lookup_object('toolhead').get_kinematics()
+        self.phoming = self.printer.lookup_object("homing")
+        kin = self.printer.lookup_object("toolhead").get_kinematics()
         for stepper in kin.get_steppers():
-            if stepper.is_active_axis('z'):
+            if stepper.is_active_axis("z"):
                 self.add_stepper(stepper)
 
     def control_fans(self, disable=True):
@@ -163,7 +186,7 @@ class ADXL345Probe:
     def probe_prepare(self, hmove, xy_homing=False):
         self.activate_gcode.run_gcode_from_command()
         chip = self.adxl345
-        toolhead = self.printer.lookup_object('toolhead')
+        toolhead = self.printer.lookup_object("toolhead")
         toolhead.flush_step_generation()
         toolhead.dwell(ADXL345_REST_TIME)
         print_time = toolhead.get_last_move_time()
@@ -171,17 +194,19 @@ class ADXL345Probe:
         chip.set_reg(REG_INT_ENABLE, 0x00, minclock=clock)
         chip.read_reg(REG_INT_SOURCE)
         chip.set_reg(REG_INT_ENABLE, 0x40, minclock=clock)
-        self.is_measuring = (chip.read_reg(adxl345.REG_POWER_CTL) == 0x08)
+        self.is_measuring = chip.read_reg(adxl345.REG_POWER_CTL) == 0x08
         if not self.is_measuring:
             chip.set_reg(adxl345.REG_POWER_CTL, 0x08, minclock=clock)
         if not self._try_clear_tap():
-            raise self.printer.command_error("ADXL345 tap triggered before move, it may be set too sensitive.")
+            raise self.printer.command_error(
+                "ADXL345 tap triggered before move, it may be set too sensitive."
+            )
         if xy_homing or not self._in_multi_probe:
             self.control_fans(disable=True)
 
     def probe_finish(self, hmove, xy_homing=False):
         chip = self.adxl345
-        toolhead = self.printer.lookup_object('toolhead')
+        toolhead = self.printer.lookup_object("toolhead")
         toolhead.dwell(ADXL345_REST_TIME)
         print_time = toolhead.get_last_move_time()
         clock = chip.mcu.print_time_to_clock(print_time)
@@ -190,7 +215,9 @@ class ADXL345Probe:
             chip.set_reg(adxl345.REG_POWER_CTL, 0x00)
         self.deactivate_gcode.run_gcode_from_command()
         if not self._try_clear_tap():
-            raise self.printer.command_error("ADXL345 tap triggered after move, it may be set too sensitive.")
+            raise self.printer.command_error(
+                "ADXL345 tap triggered after move, it may be set too sensitive."
+            )
         if xy_homing or not self._in_multi_probe:
             self.control_fans(disable=False)
 
@@ -198,13 +225,14 @@ class ADXL345Probe:
 
     def cmd_SET_ACCEL_PROBE(self, gcmd):
         chip = self.adxl345
-        self.tap_thresh = gcmd.get_float('TAP_THRESH', self.tap_thresh,
-                                         minval=TAP_SCALE, maxval=100000.)
-        self.tap_dur = gcmd.get_float('TAP_DUR', self.tap_dur,
-                                      above=DUR_SCALE, maxval=0.1)
+        self.tap_thresh = gcmd.get_float(
+            "TAP_THRESH", self.tap_thresh, minval=TAP_SCALE, maxval=100000.0
+        )
+        self.tap_dur = gcmd.get_float(
+            "TAP_DUR", self.tap_dur, above=DUR_SCALE, maxval=0.1
+        )
         chip.set_reg(REG_THRESH_TAP, int(self.tap_thresh / TAP_SCALE))
         chip.set_reg(REG_DUR, int(self.tap_dur / DUR_SCALE))
-
 
 
 def load_config(config):
